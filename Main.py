47baf30f740e250867f1f4efcc530eda5f0d779e
TongThuyVy12345 <102898659+TongThuyVy12345@users.noreply.github.com>
2023-05-25 22:46:53 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 from VHealth import app
+from VHealth.model.admin import UserView, QuestionView, AnswerView, CategoryView, MBTIView, HollandView, \
+    QuestionAnswerView, ExecriseView
 from flask import Flask, render_template, request, jsonify, session
 from VHealth.Text_Predict import Text_classification_predict
-from VHealth.Text_Predict.Text_classification_predict import TextClassificationPredict
 from VHealth.Text_Predict.Text_classification_predict import TextClassificationPredict
 from VHealth.model.LDA import make_texts_corpus
 from VHealth.model.SVM import SVM_Model
@@ -22,6 +23,8 @@ from datetime import datetime
 from VHealth.model import numerology
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin, AdminIndexView
+from flask_pymongo import PyMongo
+
 import numpy as np
 import pandas as pd
 import os
@@ -31,8 +34,20 @@ import gensim  # noqa
 import joblib  # noqa
 import VHealth.common.Settings
 import time
-app = Flask(__name__)
-admin = Admin(app, name="Admin Vhealth")
+import pymongo
+import bcrypt
+
+
+#
+# conn = pymongo.MongoClient('mongodb://localhost:27017/')
+# db = conn['database']
+
+
+# mongo = PyMongo(app)
+
+
+# client = MongoClient('mongodb://localhost:27017/')
+# db = client['database']
 
 
 @app.route("/")
@@ -92,87 +107,95 @@ def tempale_holland():
 def holland():
     # Get the message from the request
     data = request.json
-    msg = data.get('message')
-    # Initialize variables to store total scores
+    mess = data.get('mess')
     total_scoreA, total_scoreB, total_scoreC, total_scoreD, total_scoreE, total_scoreF = 0, 0, 0, 0, 0, 0
-    # If the message is valid, return the next question
-    if msg:
+    if mess:
         # Load JSON file
         with open('C:\\Users\\ADMIN\\PycharmProjects\\ModelQA_NCKH\\VHealth\\data\\JSON\\HOLLAND_JSON.json', 'r',
                   encoding="utf8") as json_data:
             intents = json.load(json_data)
-
-        # Create a Pandas DataFrame from the JSON data
         df = pd.DataFrame(intents)
-        luachon1 = "Chưa bao giờ đúng"
-        luachon2 = "Đúng trong 1 vài trường hợp"
-        luachon3 = "Đúng trong đa số các trường hợp"
-        luachon4 = "Đúng trong tất cả trường hợp"
-        # Get data input
+        max_index = len(df) - 1
+        index = session.get("index", 0)
         for i in range(len(df)):
             if df['Label'][i] == 'A':
-                total_scoreA += int(msg)
+                if mess==0:
+                    total_scoreA +=0
+                if mess==1:
+                    total_scoreA+=1
+                if mess == 2:
+                    total_scoreA += 2
+                if mess==3:
+                    total_scoreA += 3
             if df['Label'][i] == 'B':
-                total_scoreA += int(msg)
+                if mess==0:
+                    total_scoreB +=0
+                if mess==1:
+                    total_scoreB+=1
+                if mess == 2:
+                    total_scoreB += 2
+                if mess==3:
+                    total_scoreB += 3
             if df['Label'][i] == 'C':
-                total_scoreC += int(msg)
+                if mess==0:
+                    total_scoreC +=0
+                if mess==1:
+                    total_scoreC+=1
+                if mess == 2:
+                    total_scoreC += 2
+                if mess==3:
+                    total_scoreC += 3
             if df['Label'][i] == 'D':
-                total_scoreD += int(msg)
+                if mess==0:
+                    total_scoreD +=0
+                if mess==1:
+                    total_scoreD+=1
+                if mess == 2:
+                    total_scoreD += 2
+                if mess==3:
+                    total_scoreD += 3
             if df['Label'][i] == 'E':
-                total_scoreE += int(msg)
+                if mess==0:
+                    total_scoreE +=0
+                if mess==1:
+                    total_scoreE+=1
+                if mess == 2:
+                    total_scoreE += 2
+                if mess==3:
+                    total_scoreE += 3
             if df['Label'][i] == 'F':
-                total_scoreF += int(msg)
+                if mess==0:
+                    total_scoreF +=0
+                if mess==1:
+                    total_scoreF+=1
+                if mess == 2:
+                    total_scoreF += 2
+                if mess==3:
+                    total_scoreF += 3
         max_score = max(total_scoreA, total_scoreB, total_scoreC, total_scoreD, total_scoreE, total_scoreF)
-        # Get the current index from the session
-        max_index = len(df) - 1
+        if (index >= max_index):
+            if(max_score==total_scoreA):
+                content_max="Chào bạn, chào mừng bạn đến với trắc nghiệm nghề nghiệp Holland.Đầu tiên cảm ơn bạn đã tham gia thử nghiệm trắc nghiệm. Kết quả của bạn sẽ được trả chi tiết ngay sau đây: BẢNG A: " \
+                            "Realistic - Người thực tế. Người thuộc nhóm sở thích nghề nghiệp này thường có khả năng về: kỹ thuật, công nghệ, hệ thống; ưa thích làm việc với đồ vật, máy móc, động thực vật; thích làm các công việc ngoài trời. Ngành nghề phù hợp với nhóm này bao gồm: Các nghề về kiến trúc, an toàn lao động, nghề mộc, xây dựng, thủy  sản, kỹ thuật, máy tàu thủy, lái xe, huấn luyện viên, nông – lâm nghiệp (quản lý trang trại, nhân giống cá, lâm nghiệp…), cơ khí (chế tạo máy, bảo trì và sữa chữa thiết bị, luyện kim, cơ khí ứng dụng, tự động...), điện - điện tử, địa lý - địa chất (đo đạc, vẽ bản đồ địa chính), dầu khí, hải dương học, quản lý công nghiệp.."
+            elif (max_score == total_scoreB):
+                content_max = "Chào bạn, chào mừng bạn đến với trắc nghiệm nghề nghiệp Holland. Đầu tiên cảm ơn bạn đã tham gia thử nghiệm trắc nghiệm. Kết quả của bạn sẽ được trả chi tiết ngay sau đây:BẢNG B: Investigative - Người nghiên cứu.Có khả năng về: quan sát, khám phá, phân tích đánh giá và giải quyết các vấn đề. Ngành nghề phù hợp với nhóm này bao gồm: Các ngành thuộc lĩnh vực khoa học tự nhiên (toán, lý, hóa, sinh, địa lý, địa chất, thống kê…); khoa học xã hội (nhân học, tâm lý, địa lý…); y - dược (bác sĩ gây mê, hồi sức, bác sĩ phẫu thuật, nha sĩ…); khoa học công nghệ (công nghệ thông tin, môi trường, điện, vật lý kỹ thuật, xây dựng…); nông lâm (nông học, thú y…)"
 
-        index = session.get("index", 0)
-        if index >= max_index:
-            if max_score == total_scoreA:
-                content_max = "Chào bạn, chào mừng bạn đến với trắc nghiệm nghề nghiệp Holland. Đầu tiên cảm ơn bạn đã tham gia thử nghiệm trắc nghiệm. Kết quả của bạn sẽ được trả chi tiết ngay sau đây: " \
-                              "BẢNG A: Realistic - Người thực tế" \
-                              "Người thuộc nhóm sở thích nghề nghiệp này thường có khả năng về: kỹ thuật, công nghệ, hệ thống; ưa thích làm việc với đồ vật, máy móc, động thực vật; thích làm các công việc ngoài trời. Ngành nghề phù hợp với nhóm này bao gồm: Các nghề về kiến trúc, an toàn lao động, nghề mộc, xây dựng, thủy  sản, kỹ thuật, máy tàu thủy, lái xe, huấn luyện viên, nông – lâm nghiệp (quản lý trang trại, nhân giống cá, lâm nghiệp…), cơ khí (chế tạo máy, bảo trì và sữa chữa thiết bị, luyện kim, cơ khí ứng dụng, tự động...), điện - điện tử, địa lý - địa chất (đo đạc, vẽ bản đồ địa chính), dầu khí, hải dương học, quản lý công nghiệp.."
-            elif max_score == total_scoreB:
-                content_max = "Chào bạn, chào mừng bạn đến với trắc nghiệm nghề nghiệp Holland. Đầu tiên cảm ơn bạn đã tham gia thử nghiệm trắc nghiệm. Kết quả của bạn sẽ được trả chi tiết ngay sau đây: " \
-                              "BẢNG B: Investigative - Người nghiên cứu" \
-                              "Có khả năng về: quan sát, khám phá, phân tích đánh giá và giải quyết các vấn đề. Ngành nghề phù hợp với nhóm này bao gồm: Các ngành thuộc lĩnh vực khoa học tự nhiên (toán, lý, hóa, sinh, địa lý, địa chất, thống kê…); khoa học xã hội (nhân học, tâm lý, địa lý…); y - dược (bác sĩ gây mê, hồi sức, bác sĩ phẫu thuật, nha sĩ…); khoa học công nghệ (công nghệ thông tin, môi trường, điện, vật lý kỹ thuật, xây dựng…); nông lâm (nông học, thú y…)"
-            elif max_score == total_scoreC:
+            elif (max_score == total_scoreC):
+                content_max = "Chào bạn, chào mừng bạn đến với trắc nghiệm nghề nghiệp Holland. Đầu tiên cảm ơn bạn đã tham gia thử nghiệm trắc nghiệm. Kết quả của bạn sẽ được trả chi tiết ngay sau đây:BẢNG C: Artistic - Người có tính nghệ sĩ.Có khả năng về: nghệ thuật, khả năng về trực giác, khả năng tưởng tượng cao, thích làm việc trong các môi trườn mang tính ngẫu hứng, không khuôn mẫu.Ngành nghề phù hợp với nhóm này bao gồm: Các ngành về văn chương; báo chí (bình luận viên, dẫn chương trình…); điện ảnh, sân khấu, mỹ thuật, ca nhạc, múa, kiến trúc, thời trang, hội họa, giáo  viên dạy sử/Anh văn, bảo tàng, bảo tồn..."
+            elif (max_score == total_scoreD):
+                content_max = "Chào bạn, chào mừng bạn đến với trắc nghiệm nghề nghiệp Holland. Đầu tiên cảm ơn bạn đã tham gia thử nghiệm trắc nghiệm. Kết quả của bạn sẽ được trả chi tiết ngay sau đây:BẢNG D: Social - Người có tính xã hội.Có khả năng về: ngôn ngữ, giảng giải, thích làm những việc như giảng dạy, cung cấp thông tin, sự chăm sóc, giúp  đỡ, hoặc huấn luyện cho người khác. Ngành nghề phù hợp với nhóm này bao gồm: sư phạm;  giảng viên; huấn luyện viên điền kinh; tư vấn - hướng nghiệp; công tác xã hội, sức khỏe cộng đồng, thuyền  trưởng, thầy tu, thư viện, bác sĩ chuyên khoa, thẩm định giá, nghiên cứu quy hoạch đô thị, kinh tế gia đình, tuyển dụng nhân sự, cảnh sát, xã hội học, bà đỡ, chuyên gia về Xquang, chuyên gia dinh dưỡng…"
+            elif (max_score == total_scoreE):
+                content_max = "Chào bạn, chào mừng bạn đến với trắc nghiệm nghề nghiệp Holland. Đầu tiên cảm ơn bạn đã tham gia thử nghiệm trắc nghiệm. Kết quả của bạn sẽ được trả chi tiết ngay sau đây:BẢNG E: Enterprising - Dám nghĩ, dám làm.Có khả năng về: kinh doanh, mạnh bạo, dám nghĩ dám làm, có thể gây ảnh hưởng, thuyết phục Người khác, có khả năng quản lý.Ngành nghề phù hợp với nhóm này bao gồm: Các ngành về quản trị kinh doanh (quản lý khách sạn, quản trị nhân sự,…), thương mại, marketing, kế toán – tài chính, luật sư, dịch vụ khách hàng, tiếp viên hàng không, thông dịch viên, pha chế rượu, kỹ sư công nghiệp (ngành kỹ thuật hệ thống công nghiệp), bác sĩ cấp cứu, quy hoạch đô thị, bếp trưởng (nấu ăn), báo chí (phóng viên, biên tập viên…)"
+            elif (max_score == total_scoreF):
                 content_max = "Chào bạn, chào mừng bạn đến với trắc nghiệm nghề nghiệp Holland. Đầu tiên cảm ơn bạn đã tham gia thử nghiệm trắc nghiệm. Kết quả của bạn sẽ được trả chi tiết ngay sau đây:" \
-                              "BẢNG C: Artistic - Người có tính nghệ sĩ" \
-                              "Có khả năng về: nghệ thuật, khả năng về trực giác, khả năng tưởng tượng cao, thích làm việc trong các môi trườn mang tính ngẫu hứng, không khuôn mẫu.Ngành nghề phù hợp với nhóm này bao gồm: Các ngành về văn chương; báo chí (bình luận viên, dẫn chương trình…); điện ảnh, sân khấu, mỹ thuật, ca nhạc, múa, kiến trúc, thời trang, hội họa, giáo  viên dạy sử/Anh văn, bảo tàng, bảo tồn..."
-            elif max_score == total_scoreD:
-                content_max = "Chào bạn, chào mừng bạn đến với trắc nghiệm nghề nghiệp Holland. Đầu tiên cảm ơn bạn đã tham gia thử nghiệm trắc nghiệm. Kết quả của bạn sẽ được trả chi tiết ngay sau đây:" \
-                              "BẢNG D: Social - Người có tính xã hội " \
-                              "Có khả năng về: ngôn ngữ, giảng giải, thích làm những việc như giảng dạy, cung cấp thông tin, sự chăm sóc, giúp  đỡ, hoặc huấn luyện cho người khác. Ngành nghề phù hợp với nhóm này bao gồm: sư phạm;  giảng viên; huấn luyện viên điền kinh; tư vấn - hướng nghiệp; công tác xã hội, sức khỏe cộng đồng, thuyền  trưởng, thầy tu, thư viện, bác sĩ chuyên khoa, thẩm định giá, nghiên cứu quy hoạch đô thị, kinh tế gia đình, tuyển dụng nhân sự, cảnh sát, xã hội học, bà đỡ, chuyên gia về Xquang, chuyên gia dinh dưỡng…"
-            elif max_score == total_scoreE:
-                content_max = "Chào bạn, chào mừng bạn đến với trắc nghiệm nghề nghiệp Holland. Đầu tiên cảm ơn bạn đã tham gia thử nghiệm trắc nghiệm. Kết quả của bạn sẽ được trả chi tiết ngay sau đây:" \
-                              "BẢNG E: Enterprising - Dám nghĩ, dám làm" \
-                              "Có khả năng về: kinh doanh, mạnh bạo, dám nghĩ dám làm, có thể gây ảnh hưởng, thuyết phục Người khác, có khả năng quản lý.Ngành nghề phù hợp với nhóm này bao gồm: Các ngành về quản trị kinh doanh (quản lý khách sạn, quản trị nhân sự,…), thương mại, marketing, kế toán – tài chính, luật sư, dịch vụ khách hàng, tiếp viên hàng không, thông dịch viên, pha chế rượu, kỹ sư công nghiệp (ngành kỹ thuật hệ thống công nghiệp), bác sĩ cấp cứu, quy hoạch đô thị, bếp trưởng (nấu ăn), báo chí (phóng viên, biên tập viên…)"
-            elif max_score == total_scoreF:
-                content_max = "Chào bạn, chào mừng bạn đến với trắc nghiệm nghề nghiệp Holland. Đầu tiên cảm ơn bạn đã tham gia thử nghiệm trắc nghiệm. Kết quả của bạn sẽ được trả chi tiết ngay sau đây:" \
-                              "BẢNG F: Conventional - Người công chức" \
-                              "Có khả năng về: số học, thích thực hiện những công việc chi tiết, thích làm việc với những số liệu,  theo chỉ dẫn của người khác hoặc các công việc văn phòng. Ngành nghề phù hợp với nhóm này bao gồm: Các ngành nghề về hành chính, thống kê, thanh tra ngành, người giữ trẻ, điện thoại viên..."
+                                              "BẢNG F: Conventional - Người công chức.Có khả năng về: số học, thích thực hiện những công việc chi tiết, thích làm việc với những số liệu,  theo chỉ dẫn của người khác hoặc các công việc văn phòng. Ngành nghề phù hợp với nhóm này bao gồm: Các ngành nghề về hành chính, thống kê, thanh tra ngành, người giữ trẻ, điện thoại viên..."
+            return jsonify({'success': True, 'mess':mess,'testquestion':content_max,'c1':'Chưa bao giờ đúng','c2':'Đúng trong 1 vài trường hợp','c3':'Đúng trong đa số các trường hợp','c4':'Đúng trong tất cả trường hợp'}), 200
+        index += 1
+        question = df.iloc[index]['Question']
+        session.clear()
+        session["index"] = index
+    return jsonify({'success': True, 'mess':mess,'testquestion':question,'c1':'Chưa bao giờ đúng','c2':'Đúng trong 1 vài trường hợp','c3':'Đúng trong đa số các trường hợp','c4':'Đúng trong tất cả trường hợp'}), 200
 
-            return jsonify(
-                {'success': True, 'message': content_max, 'labelA': total_scoreA, 'labelB': total_scoreB,
-                 'labelC': total_scoreC, 'labelB': total_scoreD, 'labelE': total_scoreE, 'labelF': total_scoreF,
-                 'choice': luachon1, 'choice2': luachon2, 'choice3': luachon3, 'choice4': luachon4}), 200
-        else:
-            index += 1
-            question = df.iloc[index]['Question']
-
-            # Clear the session once the quiz is complete
-            session.clear()
-            # Store the updated index in the session
-            session["index"] = index
-        # Return the question as a JSON response, along with the updated index
-        return jsonify(
-            {'success': True, 'message': question, 'index': index, 'msg': msg, 'choice': luachon1, 'choice2': luachon2,
-             'choice3': luachon3, 'choice4': luachon4}), 200
-
-        # If the message is invalid, return an error response
-    else:
-        return jsonify({'success': False, 'message': 'Dữ liệu không hợp lệ'}), 400
 
 
 @app.route("/quizMBTI")
@@ -295,10 +318,26 @@ def login():
     return render_template('login.html')
 
 
-# Register
-@app.route("/register")
-def register():
-    return render_template("register.html")
+
+# # Register
+# @app.route("/register", methods=["GET", "POST"])
+# def register():
+#     if request.method == "POST":
+#         # Get the form data
+#         username = request.form.get("username")
+#         password = request.form.get("password")
+#         fullname = request.form.get("username")
+#         phone = request.form.get("phone")
+#         email = request.form.get("email")
+#         gender = request.form.get("gender")
+#         hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+#         print("Received password:", password)
+# 
+#         db['User'].insert_one(
+#             {"password": hashed_password, "email": email, "fullname": fullname, "gender": gender, "issuperuser": 1,
+#              "phone": phone, "username": username})
+# 
+#     return render_template("register.html")
 
 
 # Numberology
@@ -314,5 +353,16 @@ def get_bot_response():
     )
 
 
+#
+#
+# admin = Admin(app, name="Admin Vhealth")
+# admin.add_view(UserView(db['User'], 'User'))
+# admin.add_view(QuestionView(db['Question'], 'Question'))
+# admin.add_view(AnswerView(db['Answer'], 'Answer'))
+# admin.add_view(CategoryView(db['Category'], 'Category'))
+# admin.add_view(MBTIView(db['MBTI'], 'MBTI'))
+# admin.add_view(HollandView(db['Holland'], 'Holland'))
+# admin.add_view(QuestionAnswerView(db['Question_Answer'], 'Question Answer'))
+# admin.add_view(ExecriseView(db['Execrise'], 'Execrise'))
 if __name__ == '__main__':
     app.run(debug=True)
