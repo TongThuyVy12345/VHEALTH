18811039fa611697a3bf25d9514477f282ab036b
TongThuyVy12345 <102898659+TongThuyVy12345@users.noreply.github.com>
2023-05-27 15:52:44 +0700

    Update Text_classification_predict.py
    fix config

--- Text_Predict/Text_classification_predict.py
+++ Text_Predict/Text_classification_predict.py
@@ -1,42 +1,29 @@
-from VHealth.common import File
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+import os
 import pandas as pd
-import random
-from VHealth.model.SVM import SVM_Model
+def Text_Predict(query):
+    with open('C:\\Users\ADMIN\PycharmProjects\ModelQA_NCKH\VHealth\svm_model.pkl', 'rb') as file:
+        svm_model = pickle.load(file)
+    def get_parentDir():
+        fileDir = os.path.dirname(os.path.abspath(__file__))
+        parentDir = os.path.dirname(fileDir)
+        return parentDir
 
-class TextClassificationPredict(object):
-    def __init__(self, question_test, db_train, db_train_extend, db_answers):
-        self.model=self.select_model(1)
-        self.question_test = question_test
-        self.db_train = db_train
-        self.db_answers = db_answers
-        self.db_train_extend = db_train_extend
+    csv_path = os.path.join(get_parentDir(), 'data', 'CSV', 'FINAL_EN.csv')
+    df = pd.read_csv(csv_path, encoding='utf8')
 
-    def select_model(self, i_model):
-        if i_model == 1:
-            self.model = SVM_Model()
-    def Text_Predict(self):
-        fallback_intent = File.get_fallback_intent()
-        df_train_extend = pd.DataFrame(self.db_train_extend)
-        df_train = pd.DataFrame(self.db_train)
-        df_answers = pd.DataFrame(self.db_answers)  # List Answers
-        db_Predict = []
-        db_Predict.append({"Question_tokens": self.question_test})
-        df_Predict = pd.DataFrame(db_Predict)
-        # Predict in Question Text
-        self.select_model(1)
-        clf = self.model.clf.fit(df_train_extend["Question_tokens"], df_train_extend.Label)
-        list_score = clf.predict_proba(df_Predict["Question_tokens"]).flatten()  # --> array of score
-        predicted = list_score.tolist().index(list_score.max())
-
-        if (list_score[predicted] >= 0.5):
-            mess = "Answer: "
-            mess += df_answers["Answer"][predicted] \
-                    + "\n(Score:" + str(round(list_score[predicted], 3)) \
-                    + ")"
-            return mess
-        else:
-            print("Question: " + self.question_test + "\n")
-            mess = fallback_intent[random.randint(0, len(fallback_intent) - 1)]
-            return mess
-
-
+    vectorizer = TfidfVectorizer()
+    new_sentences=[query]
+    X_train = vectorizer.fit_transform(df['Question_tokens'])
+    X_new = vectorizer.transform(new_sentences)
+    predictions = svm_model.predict(X_new)
+    for sentence, prediction in zip(df['Question'], predictions):
+        similarity = "Similar" if prediction == 1 else "Dissimilar"
+        # print(f"'{sentence}' is {similarity} to the new sentence.")
+        index = df[df['Question'] == sentence].index[0]
+        answer = df.loc[index, 'Answer']
+        # print(f"Answer: {answer}")
+        return answer
+query="what is the psychology ?"
+print(Text_Predict(query))
