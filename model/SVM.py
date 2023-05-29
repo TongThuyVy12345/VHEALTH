77ce9aed05d21bbd8cd626bd2d66d303ec1fb76c
TongThuyVy12345 <102898659+TongThuyVy12345@users.noreply.github.com>
2023-05-29 22:35:52 +0700

    Update SVM.py

--- model/SVM.py
+++ model/SVM.py
@@ -1,26 +1,56 @@
-
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.svm import SVC
-from VHealth.Transformer.Feature_transformer import FeatureTransformer
-class SVM_Model(object):
-    def __init__(self):
-        self.clf = self._init_pipeline()
-
-    @staticmethod
-    def _init_pipeline():
-        pipe_line = Pipeline([
-            ("transformer", FeatureTransformer()),
-            ("vect", CountVectorizer()),
-            ("tfidf", TfidfTransformer()),
-            ("clf", SVC(kernel='linear', C=1, gamma=10.0, probability=True))
-        ])
-
-        return pipe_line
-# X = ["This is a sample document.", "Another document to test.", "Yet another document."]
-# y = [0, 1, 0]
-# import pandas as pd
-# X_series = pd.Series(X)
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import GridSearchCV
+# from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 #
-# svm_model = SVM_Model()
-# svm_model.clf.fit(X_series, y)
+# from sklearn.svm import SVC
+# from VHealth.common import File
+# import codecs
+# import os
+# import json
+# import pandas as pd
+# import json
+# import pickle
+#
+#
+# def get_parentDir():
+#     fileDir = os.path.dirname(os.path.abspath(__file__))
+#     parentDir = os.path.dirname(fileDir)
+#     return parentDir
+#
+#
+# csv_path = os.path.join(get_parentDir(), 'data', 'CSV', 'FINAL_EN.csv')
+# df = pd.read_csv(csv_path, encoding='utf8')
+#
+# # Preprocess the text data using TF-IDF vectorization
+# vectorizer = TfidfVectorizer()
+# X = vectorizer.fit_transform(df['Question_tokens'])
+#
+# # X_train, X_test, y_train, y_test = train_test_split(X, df['Label'], test_size=0.2, random_state=42)
+#
+# target_sentence = "where can i learn about types of mental health treatment?"
+# target_vector = vectorizer.transform([target_sentence])
+#
+# svm_model = SVC(kernel='linear', C=2, gamma=0.1, probability=True)
+# svm_model.fit(X, range(len(df['Question_tokens'])))
+# # model_path = os.path.join(get_parentDir(), 'model', 'svm_model.pkl')
+# # with open(model_path, 'wb') as file:
+# #     pickle.dump(svm_model, file)
+# predicted_index = svm_model.predict(target_vector)[0]
+# most_similar_sentence = df['Question_tokens'][predicted_index]
+# print("Câu giống nhất:", most_similar_sentence)
+# # accuracy = svm_model.score(X_test, y_test)
+# # print("Accuracy:", accuracy)
+# #
+# # # Calculate and print F1 score
+# # y_pred = svm_model.predict(X_test)
+# # f1 = f1_score(y_test, y_pred, average='weighted')
+# # print("F1 Score:", f1)
+# #
+# # # Calculate and print precision
+# # precision = precision_score(y_test, y_pred, average='weighted')
+# # print("Precision:", precision)
+# #
+# # # Calculate and print recall
+# # recall = recall_score(y_test, y_pred, average='weighted')
+# # print("Recall:", recall)
